b7a0604905516bbb063b268b945b2e6ef06286ba
MaksimRadziuk <30619681+MaksimRadziuk@users.noreply.github.com>
2020-01-14 14:34:07 +0200

    14/01 15.34

--- js/common.js
+++ js/common.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+/*Открытие/Закрытие ховер контента Как это работает, Главная*/
 	$('span.trigger').click(function(){
 		$(this).siblings('.hidden_wrap').addClass('active');
 	})
@@ -10,18 +11,16 @@ $(document).ready(function(){
 	$('.hidden_wrap .close').click(function(){
 		$(this).parent('.hidden_wrap').removeClass('active');
 	})
-	$('.popup .close').click(function(){
-		$(this).closest('.popup').hide();
-		$('#popup_background').hide();
-		let reloadVideo = $('.modal-video').attr("src");
-		$('.modal-video').attr('src', reloadVideo);
-	})
+
+/*Открытие/Закрытие мобильного меню*/
 	$('#mobileMenuTrigger').click(function(){
 		$('#mobileMenu').toggleClass('active');
 	})
 	$('#mobileMenu .close').click(function(){
 		$('#mobileMenu').toggleClass('active');
 	})
+
+/*Управление табами Как это работает, Главная*/
 	$('#self_trade-button').click(function(){
 		$(this).addClass('active');
 		$('#auto_trade-button').removeClass('active');
@@ -37,7 +36,7 @@ $(document).ready(function(){
 		$('.hidden_wrap').removeClass('active');
 	})
 
-
+/*Слайдер Видео на Главной*/
 	$('.videos_slider-wrap').slick({
 		slidesToShow: 3,
   		slidesToScroll: 1,
@@ -54,13 +53,22 @@ $(document).ready(function(){
 		if ($(window).width()<961) {
 			$('.videos_slider-wrap').slick('unslick');
 		}
+		if ($(window).width()>960) {
+			$('.videos_slider-wrap').slick({
+				slidesToShow: 3,
+		  		slidesToScroll: 1,
+		  		dots:true,
+		  		arrows:true
+			})
+		}
 	})
 
-
+/*Перелистывание теста Главная*/
 	$('section.test button').click(function(){
 		$(this).closest('.test_slide','section.test').next('.test_slide').addClass('active')
 	})
 
+/*Обратный отсчет на главной странице*/
 	var clock = $('section.promo .clock_wrap').FlipClock({
 		clockFace : "DailyCounter",
 		autoStart: false,
@@ -68,7 +76,7 @@ $(document).ready(function(){
 		language: 'ru-ru'
 
 	});
-	var dt = "January 30 2020 00:00:00";
+	var dt = "January 30 2020 00:00:00"; /*Дата окончания отсчета*/
 	var first = new Date(dt);
 	var last = Date.now();
 	var remaining = first - last;
@@ -78,6 +86,7 @@ $(document).ready(function(){
 	clock.start();
 
 
+/*Попап триггеры*/
 	$('#videoPopup').click(function(){
 		$('#videoWrap').show();
 		$('#popup_background').show();
@@ -99,14 +108,41 @@ $(document).ready(function(){
 		$('#loginWindow').show();
 	})
 
+/*Закрытие Попап*/
+	$('.popup .close').click(function(){
+		$(this).closest('.popup').hide();
+		$('#popup_background').hide();
+		let reloadVideo = $('.modal-video').attr("src");
+		$('.modal-video').attr('src', reloadVideo);
+	})
 
-
+/*Скрипты на странице FAQ*/
 	$('.answer_trigger').click(function(){
 		$(this).siblings('p').toggleClass('active');
 		$(this).children('span').toggleClass('active');
 	})
 	$('.question').click(function(){
 		$(this).siblings('.answer').toggle();
+	})
+
+/*Скрипты на сранице Блог*/
+	$('#blogNews').click(function(){
+		$('.tab').removeClass('active');
+		$(this).addClass("active");
+		$('.blog_wrap').removeClass('active');
+		$('#newsWrap').addClass("active");
+	})
+	$('#blogArticle').click(function(){
+		$('.tab').removeClass('active');
+		$(this).addClass("active");
+		$('.blog_wrap').removeClass('active');
+		$('#articleWrap').addClass("active");
+	})
+	$('#blogEvents').click(function(){
+		$('.tab').removeClass('active');
+		$(this).addClass("active");
+		$('.blog_wrap').removeClass('active');
+		$('#eventsWrap').addClass("active");
 	})
 
 })
